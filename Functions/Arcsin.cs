86f574330d1ac6376613d7b61ea36b0372e1f559
Dylan Brito <dylan-brito@oultook.fr>
2022-06-21 12:28:40 +0200

    Implémentation de la fonction Arcsin

---
+++ Functions/Arcsin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBASIC.Functions
+{
+    class Arcsin : IFunction
+    {
+        public float Apply(List<BasicNumber> arguments)
+        {
+            return (float)Math.Asin(arguments[0].GetValue());
+        }
+    }
+}
